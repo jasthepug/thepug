e71573e6301357e797b4933bb2ce1e782c68ef95
Abinash Shrestha <abinash@Abinashs-MacBook-Pro.local>
2017-10-09 14:55:05 +1100

    DV-1 setup

--- wp-content/themes/pugfinancial/index.php
+++ wp-content/themes/pugfinancial/index.php
@@ -13,169 +13,159 @@
 
 get_header(); ?>
 
-	<div class="row">
-		<div class="col-sm-11 col-sm-offset-1 col-xs-12 text-xs-center "  >
-			
-			<img style="max-width:250px;margin-top:20px;" src="<?php echo get_template_directory_uri(); ?>/img/logo1.jpg">
-		</div>
-	</div>
-	
-	<div class="row hidd">
-		<div class="col-xs-12 text-xs-center "  >
-			
-			<img style="max-width:180px;" src="<?php echo get_template_directory_uri(); ?>/img/pug_med.jpg">
-		</div>
-	</div>
-	
-	<div class="row">
-		<div class="col-sm-11 col-sm-offset-1 col-xs-12 carosello maxtxt" >
-			
-			<div id="myCarousel" class="carousel slide" data-ride="carousel">
-			  <!-- Indicators -->
-			  
-
-			  <!-- Wrapper for slides -->
-			  <div class="carousel-inner" role="listbox" style="height:380px;">
-				<div class="item active " >
-				   <div class="cont-carousel  ">
-						<br /><p class="ttle" >Hi, I'm the Pug...</p><br />
-						and you are likely paying more than you should  on your home loan!
-						
-						<br />
-						<a href="loan/low-rate" class=" btn  buttt" style="margin-top:20px;">FIND OUT HOW TO SAVE</a>
-					</div>
-				</div>
-
-				<div class="item">
-				<div class="cont-carousel">
-				    <br /><p class="ttle" >Bank said "No"?</p><br />
-					
-				The Pug tries hard to turn that into a "Yes"!<br />
-				
-				<a  href="loan/bank-said-no" class=" btn  buttt" style="margin-top:20px;">FIND OUT MORE </a>
-				</div>
-				</div>
-
-				<div class="item">
-				<div class="cont-carousel">
-				  <br /><p class="ttle" >Our Rate Challenge</p><br />
-				   Send us your current rate and we'll reply with our best rate<br />
-						<a href="/rate-challenge" class=" btn  buttt" style="margin-top:20px;">START NOW!</a>
-				</div>
-
-				</div>
-				
-				<div style="width:100px;position:absolute;top:360px;right:10px;" >
-					<ol class="carousel-indicators">
-					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-					<li data-target="#myCarousel" data-slide-to="1"></li>
-					<li data-target="#myCarousel" data-slide-to="2"></li>
-					
-					</ol>
-				</div>
-			  </div>
-
-			  <!-- Left and right controls -->
-			  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-				<br /><br /><br /><img src="<?php echo get_template_directory_uri(); ?>/img/arrowl.gif" class="frecc" />
-				<span class="sr-only">Previous</span>
-			  </a>
-			  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-				<br /><br /><br /><img src="<?php echo get_template_directory_uri(); ?>/img/arrowr.gif" class="frecc" />
-				<span class="sr-only">Next</span>
-			  </a>
-			</div>
-		</div>
-		
-		
-	</div>
-	
-	
-   <div class="row">
-		<div class="col-sm-12 col-xs-12 bblue" >
-				<div class="col-sm-11 col-sm-offset-1 col-xs-12 maxtxt" >
-					
-					The Pug is a mortgage broker, which means we are a licensed service that helps people find better home loan deals.
-					As a mortgage broker, we have access to special deals and promotions from most lenders, including the big four banks.
-					We assist with every step of the loan process, from calculating how much you can borrow, to finding great deals, through to 
-					completing the application forms.
-					We also help beyond that by readily 
-					answering any questions you may have when your needs change.
-					<br /><br />
-					<strong>Best of all, our service is free!</strong>
-					<br /><br /><br />
-					
-				</div>	
-				
-				<div class="col-sm-5 col-sm-offset-2 col-xs-12 " >
-					<p class="ttlb">WHAT WE DO</p><br />
-					<ul class="tick">
-							<li>Contact multiple lenders to find and negotiate better rates on your behalf</li>
-
-							 <li>Assist with all the paperwork</li>
-
-							 <li>Calculate borrowing capacity, repayments and how to save</li>
-					</ul><br /><br />
-				</div>	
-				<div class="col-sm-5  col-xs-12 " >
-					<p class="ttlb">WHAT WE WON'T DO</p><br />
-					<ul class="ics">
-							<li> Charge you any fees</li>
-
-							 <li> Just compare rates like most comparison sites</li>
-
-							<li> Flick you off to an overseas call centre</li>
-
-					 </ul>
-				</div>	
-		</div>
-	</div>
-	
-	
-	<div class="row">
-		<div class="col-sm-12 col-xs-12 ggrey" >
-			<div class="col-sm-4 col-sm-offset-2 col-xs-12 " >
-					
-					Do you get the feeling you are paying 
-				too much on your loan? <br />
-				It’s really easy to find out.
-					<br /><br /><br />
-					
-			</div>	
-			<div class="col-sm-2 text-sm-center text-xs-center col-xs-12 " >
-				<div class="apc" >CALL US</div>
-			<a href="tel:1300843784" class="lnk">1300 THE PUG</a><br /><br />
-			</div>	
-			<div class="col-sm-2 text-sm-center text-xs-center col-xs-12 " >
-			<div class="apc" >EMAIL US</div>
-			<a href="mailto:info@thepug.com.au" class="lnk">INFO@THEPUG.COM.AU</a>
-			</div>	
-			<div class="col-sm-2" ></div>	
-		</div>
-	</div>
-	
-	
-   <div class="row">
-	   <div class="col-sm-12 col-xs-12" style="margin-bottom:20px;">
-	   
-			<div style="margin:0 auto; text-align:center" >
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/westpac.gif" alt="westpac" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/nab.gif" alt="nab" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/commonwealth.gif" alt="commonwealth" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/connective.gif" alt="connective" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/anz.gif" alt="anz" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/macquarie.gif" alt="macquarie" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/bluestone.gif" alt="bluestone" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/suncorp.gif" alt="suncorp" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/bankwest.gif" alt="bankwest" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/ing.gif" alt="ing" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/amp.gif" alt="amp" /> &nbsp;  &nbsp; 
-			<img  src="<?php echo get_template_directory_uri(); ?>/img/logos/boq.gif" alt="boq" /> &nbsp;  &nbsp; 
-			<br /><br />
-			</div>
-	   
-
-		</div>
-	</div>
+<section class="container">
+    <div class="row pg-intro">
+        <div class="col-sm-offset-1 col-xs-offset-3 col-xs-6 col-sm-4">
+            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/img/pug_med.jpg">
+        </div>
+        <div class="col-xs-12 col-sm-7">
+            <blockquote class="pg-intro__text">
+                <span id="intro-slogan"></span>
+            </blockquote>
+            <div class="row">
+                <div id="carousel-example-generic" class="carousel slide pg-carousel" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                    </ol>
+                    <div class="carousel-inner mt-2" role="listbox">
+                        <div class="item active">
+                            <h2 class="h4 mb-2 text-muted text-center font-family-heading">Owner Occupied Loans</h2>
+                            <div class="col-xs-12">
+                                <div class="pg-intro-rate">
+                                    <h3 class="pg-intro-rate__text">Variable Rate</h3>
+                                    <div class="pg-intro-rate__number">3.68% <span class="small">p.a.</span></div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12">
+                                <div class="pg-intro-rate">
+                                    <h3 class="pg-intro-rate__text">Comparison Rate *</h3>
+                                    <div class="pg-intro-rate__number">3.69% <span class="small">p.a.</span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <h2 class="h4 mb-2 text-muted text-center font-family-heading">Investment Loans</h2>
+                            <div class="col-xs-12">
+                                <div class="pg-intro-rate">
+                                    <h3 class="pg-intro-rate__text">Variable Rate</h3>
+                                    <div class="pg-intro-rate__number">4.09% <span class="small">p.a.</span></div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12">
+                                <div class="pg-intro-rate">
+                                    <h3 class="pg-intro-rate__text">Comparison Rate *</h3>
+                                    <div class="pg-intro-rate__number">4.25% <span class="small">p.a.</span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <h2 class="h4 mb-2 text-muted text-center font-family-heading">Owner Occupied Fixed 2Yr</h2>
+                            <div class="col-xs-12">
+                                <div class="pg-intro-rate">
+                                    <h3 class="pg-intro-rate__text">Variable Rate</h3>
+                                    <div class="pg-intro-rate__number">3.64% <span class="small">p.a.</span></div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12">
+                                <div class="pg-intro-rate">
+                                    <h3 class="pg-intro-rate__text">Comparison Rate *</h3>
+                                    <div class="pg-intro-rate__number">4.83% <span class="small">p.a.</span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <h2 class="h4 mb-2 text-muted text-center font-family-heading">Investment Fixed 2Yr</h2>
+                            <div class="col-xs-12">
+                                <div class="pg-intro-rate">
+                                    <h3 class="pg-intro-rate__text">Variable Rate</h3>
+                                    <div class="pg-intro-rate__number">3.85% <span class="small">p.a.</span></div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12">
+                                <div class="pg-intro-rate">
+                                    <h3 class="pg-intro-rate__text">Comparison Rate *</h3>
+                                    <div class="pg-intro-rate__number">4.97% <span class="small">p.a.</span></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <h2 class="h4 mb-2 text-muted text-center font-family-heading">Low Doc Loans</h2>
+                            <div class="col-xs-12">
+                                <div class="pg-intro-rate">
+                                    <h3 class="pg-intro-rate__text">Variable Rate</h3>
+                                    <div class="pg-intro-rate__number">4.04% <span class="small">p.a.</span></div>
+                                </div>
+                            </div>
+                            <div class="col-xs-12">
+                                <div class="pg-intro-rate">
+                                    <h3 class="pg-intro-rate__text">Comparison Rate *</h3>
+                                    <div class="pg-intro-rate__number">4.23% <span class="small">p.a.</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                        <span class="fa fa-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                        <span class="fa fa-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+                <div class="mt-8 pg-apply-now">
+                    <a href="<?php echo get_site_url(); ?>/find-out-more"
+                       class="btn btn-primary btn-lg btn-round pg-btn-wide-padding mr-3">Apply Now</a>
+                    <div class="mr-3">Or</div>
+                    <div>
+                        <h1 class="mb-1 h5">
+                            <span class="text-primary inline-block vertical-align-middle big mr-1"><i
+                                        class="fa fa-phone"></i></span>
+                            <span class="inline-block vertical-align-middle font-family-base">Call Us Now</span>
+                        </h1>
+                        <h2 class="text-primary font-family-base mt-0 h4">1300 THE PUG</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="pg-bank-showcase">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/westpac.gif" alt="westpac"
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/nab.gif" alt="nab"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/commonwealth.gif" alt="commonwealth"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/connective.gif" alt="connective"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/anz.gif" alt="anz"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/macquarie.gif" alt="macquarie"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/bluestone.gif" alt="bluestone"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/suncorp.gif" alt="suncorp"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/bankwest.gif" alt="bankwest"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/ing.gif" alt="ing"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/amp.gif" alt="amp"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/boq.gif" alt="boq"/>
+            </div>
+        </div>
+    </div>
+</section>
+<article class="bg-lightest">
+    <h2 class="sr-only">DISCLAIMER</h2>
+    <div class="container text-muted pt-4 pb-4 small">
+        *The comparison rates are based on a loan amount of $150,000 and a term of 25 years. Warning: These comparison
+        rates apply only to the example or examples given. Different amounts and terms will result in different
+        comparison rates. Costs such as redraw fees or early repayment fees, and costs savings such as fee waivers, are
+        not included in the comparison rate but may influence the cost of the loan. All offers and products shown on
+        this website are subject to lender approval, terms and conditions. Please contact The Pug for key details
+        regarding these terms and conditions. Your full financial situation would need to be reviewed prior to
+        acceptance of any offer or product. All rates, deals and offers are believed to be correct at the time of
+        publication, but are subject to final confirmation by lender and may change without notice.
+    </div>
+</article>
 
 <?php get_footer(); ?>
